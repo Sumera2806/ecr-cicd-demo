bd6e0cb27bda6ba10bf36adb74e052eebd1841ff
Syeda Sumera <syedasumera366y@gmail.com>
2026-01-08 20:13:20 +0530

    Initialize Flask app with health check endpoint

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+from datetime import datetime
+import socket
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify({
+        "message": "CI/CD Pipeline Success ",
+        "service": "GitHub Actions → ECR → EC2",
+        "hostname": socket.gethostname(),
+        "timestamp": datetime.utcnow().isoformat() + "Z"
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
